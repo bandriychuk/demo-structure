8fd24889f19979e1c063966895ede98c548b9ec7
bandriychuk <andriychuk23@gmail.com>
2021-05-25 16:14:26 +0300

    update jenkinsfile

--- ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
+++ ui-test/src/test/java/com/socks/ui/tests/BaseUITest.java
@@ -13,7 +13,7 @@ public class BaseUITest {
 	@BeforeClass
 	public void setUp(){
 		ProjectConfig config = ConfigFactory.create(ProjectConfig.class);
-		RestAssured.baseURI = config.baseUrl();
+		RestAssured.baseURI = config.prodEnv();
 		Configuration.browser  = "com.socks.ui.SelenoidDriverProvider";
 	}
 
